1f3dbbf006b36d07afeb5cb091108690ceab4e6a
KimJiYeon <jsujyeon@naver.com>
2023-09-10 15:01:26 +0900

    first_commit

---
+++ src/main/java/com/study/board/MyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.study.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBoardApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/study/board/repository/BoardRepository.java
@@ -0,0 +1,14 @@
+package com.study.board.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.study.board.entity.Board;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+
+	Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
+}
