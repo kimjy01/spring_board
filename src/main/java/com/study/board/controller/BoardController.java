c34f13f1f9fa5f88225ba350231d5f162550ebff
KimJiYeon <jsujyeon@naver.com>
2023-10-10 19:15:37 +0900

    board_download

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,10 +1,19 @@
 package com.study.board.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +23,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
-
 
 
 @Controller
@@ -53,15 +61,18 @@ public class BoardController {
 			list = boardService.boardSearchList(searchKeyword, pageable);
 		}
 		
-		
 		int nowPage = list.getPageable().getPageNumber() +1;
 		int startPage = Math.max(nowPage - 4, 1);
 		int endPage = Math.min(nowPage + 5, list.getTotalPages());
+		
+		int totalPage = list.getTotalPages();
 		
 		model.addAttribute("list", list);
 		model.addAttribute("nowPage", nowPage);
 		model.addAttribute("startPage", startPage);
 		model.addAttribute("endPage", endPage);
+		model.addAttribute("totalPage", totalPage);
+		
 		
 		return "boardList";
 		
@@ -106,5 +117,28 @@ public class BoardController {
 		
 		return "message";
 	}
+	
+	@GetMapping("/board/download/{fileName:.+}")
+    public ResponseEntity<byte[]> downloadFile(@PathVariable String fileName) {
+        // 파일을 저장하는 경로
+        String fileStoragePath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+        Path filePath = Paths.get(fileStoragePath).resolve(fileName);
+
+        try {
+            byte[] fileContent = Files.readAllBytes(filePath);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setContentLength(fileContent.length);
+            headers.setContentDispositionFormData("attachment", fileName);
+
+            return new ResponseEntity<>(fileContent, headers, org.springframework.http.HttpStatus.OK);
+        } catch (IOException e) {
+            // 파일을 찾을 수 없거나 읽을 수 없는 경우에 대한 예외 처리
+            e.printStackTrace();
+            return new ResponseEntity<>(null, org.springframework.http.HttpStatus.NOT_FOUND);
+        }
+    }
+	
 }
 
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -6,11 +6,16 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
+
+import jakarta.annotation.Resource;
 
 @Service
 public class BoardService {
